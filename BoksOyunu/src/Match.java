fe99474dcf69a0ca850f9c03909eb40f45322fb8
Onat <97096172+onatkarabulut@users.noreply.github.com>
2022-04-13 00:25:57 +0300

    Update Match.java

--- BoksOyunu/src/Match.java
+++ BoksOyunu/src/Match.java
@@ -1,9 +1,11 @@
 public class Match {
+
     Fighter f1;
     Fighter f2;
     int minWeight;
     int maxWeight;
-
+    int round=0;
+    int whoHits=0;
 
     public Match(Fighter f1, Fighter f2, int minWeight, int maxWeight) {
         this.f1 = f1;
@@ -12,45 +14,87 @@ public class Match {
         this.maxWeight = maxWeight;
     }
 
-    public void run(){
-        if (isCheck()) {
-            while  (this.f1.health>0 && this.f2.health>0){
-                if (this.f1.isStart() && this.f2.isStart()){
-                    System.out.println("=====>>> ROUND <<<====");
-                    if (this.f1.isStart()){
-                        f2.health=f1.hit(f2);
-                        if (isWin()){
-                            break;
-                        }
-                    }else{
-                        f1.health=f2.hit(f1);
-                        if (isWin()){
-                            break;
-                        }
+    public void run() {
+
+        if (checkWeight()) {
+            while (f1.health > 0 && f2.health > 0) {
+                f1.damage = (int)(Math.random()*f1.maxDamage);
+                f2.damage=(int)(Math.random()*f1.maxDamage);
+
+                //int currentDamage2 = Math.random()*f2.damage;
+
+                double whoStarts = Math.random() * 100;  //0.0 to 99.9
+                System.out.println("======== YENİ ROUND ("+this.round+") =========== ");
+
+
+                if (whoStarts > 50 && round == 0) { //ilk round ise ve 50 ustundeyse f2 , 50 altindaysa f1 baslasin
+                    f2.health = f1.hit(f2);
+                    whoHits = 1;
+                    if (isWin()) {
+                        break;
                     }
-                    System.out.printf("Canlar\nSavasci : %s  | Can : %d\nSavasci : %s  | Can : %d\n",f1.name  ,f1.health,f2.name,f2.health);
+                } else {
+                    f1.health = f2.hit(f1);
+                    whoHits = 2;
+                    if (isWin()) {
+                        break;
+                    }
+
                 }
 
+                if(whoHits==1){
+                    f1.health = f2.hit(f1);
+                    whoHits=2;
+                    if (isWin()){
+                        break;
+                    }
+                }else{
+                    f2.health = f1.hit(f2);
+                    whoHits=1;
+                    if (isWin()){
+                        break;
+                    }
+                }
+
+
+
+
+
+                round++;
+
+                printScore();
             }
 
-        }else {
-            System.out.println("Sporcular maça giremez...");
+        } else {
+            System.out.println("Sporcuların ağırlıkları uyuşmuyor.");
         }
+
+
     }
 
-    public boolean isCheck(){
-        return (this.f1.weight>=minWeight && this.f1.weight<=maxWeight) && (this.f2.weight>=minWeight && this.f2.weight<=maxWeight);
+    public boolean checkWeight() {
+        return (f1.weight >= minWeight && f1.weight <= maxWeight) && (f2.weight >= minWeight && f2.weight <= maxWeight);
     }
 
-    public boolean isWin(){
-        if(this.f1.health==0){
-            System.out.println(f2.name + " kazandı.");
+    public boolean isWin() {
+        if (f1.health == 0) {
+            System.out.println("Maçı Kazanan : " + f2.name);
+            printScore();
             return true;
-        }else if(this.f2.health==0){
-            System.out.println(f1.name + " kazandı.");
+        } else if (f2.health == 0){
+            System.out.println("Maçı Kazanan : " + f1.name);
+            printScore();
             return true;
         }
+
         return false;
     }
 
+
+    public void printScore() {
+        System.out.println("------------");
+
+        System.out.println(f1.name + " Kalan Can \t:" + f1.health);
+        System.out.println(f2.name + " Kalan Can \t:" + f2.health);
+    }
 }
